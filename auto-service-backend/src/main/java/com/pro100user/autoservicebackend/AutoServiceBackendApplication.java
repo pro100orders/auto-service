944b48e77afb5f46dd0eef5caf413ab708b9a93f
pro100user <51286168+pro100user15@users.noreply.github.com>
2022-04-01 00:35:25 +0300

    inizialize
    Inizialize backend and frontend

---
+++ auto-service-backend/src/main/java/com/pro100user/autoservicebackend/AutoServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pro100user.autoservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceBackendApplication.class, args);
+	}
+
+}
---
+++ auto-service-backend/src/test/java/com/pro100user/autoservicebackend/AutoServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro100user.autoservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
